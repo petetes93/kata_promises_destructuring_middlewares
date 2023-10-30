1b1d17c567f70a5861a785295c546ad740e1de92
petetes93 <alejandrolobatohaurie@gmail.com>
2023-10-30 17:38:22 +0100

    first-commit

---
+++ iteracion1/iteracion1.js
@@ -0,0 +1,42 @@
+console.log('test')
+// !- [ ] 1.- Dado el objeto **empleados**, extrae la empleada Ana completa.
+const empleados = [
+  { name: 'Luis', email: 'Luis@gmail.com' },
+  { name: 'Ana', email: 'Ana@gmail.com' },
+  { name: 'Andrea', email: 'Andrea@gmail.com' },
+]
+
+const [, n] = empleados
+console.log(n)
+
+//!- [ ] 2.- Dado el objeto **empleados**, extrae el email del empleado Luis --> Luis@gmail.com
+const [{ email }] = empleados
+console.log(email)
+
+//! - [ ] 3.- Usa destructuración para cambiar los valores de a y b;
+
+// Inicialmente
+let a = 5
+let b = 3
+// Al final
+// let a = 3;
+// let b = 5;
+;[a, b] = [b, a]
+
+console.log(a)
+console.log(b)
+
+//!- [ ] 4.- Dado el objeto **HIGH_TEMPERATURES**
+
+const HIGH_TEMPERATURES = {
+  yesterday: 30,
+  today: 35,
+  tomorrow: 32,
+}
+
+// !  Cambiar las siguientes líneas para guardar mediante destructuración los valores de temperaturas en las variables maximaHoy y maximaManana
+
+const maximaHoy = HIGH_TEMPERATURES.today
+const maximaManana = HIGH_TEMPERATURES.tomorrow
+console.log(maximaHoy)
+console.log(maximaManana)
